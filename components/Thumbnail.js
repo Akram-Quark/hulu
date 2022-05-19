b98a5217a8157f9a62bbf026589ffb7f85011805
Bediaf Akram <88899765+Akram-Quark@users.noreply.github.com>
2022-05-19 23:05:53 +0100

    ++

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -24,8 +24,8 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.release_date || result.first_air_date}
-          {}
-          <ThumbUpIcon className="h-5 mx-2"></ThumbUpIcon>
+          
+          <ThumbUpIcon className="h-5 mx-2"/>
           {result.vote_count}
         </p>
       </div>
